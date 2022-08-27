8ab9b35521f8b8fe8f3a8505b43e16ee3f13addc
Richter Rojas <credipymesolucion@outlook.com>
2022-08-27 13:13:27 -0500

    creando proyecto vue para frontend del crud

---
+++ vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<b-table striped hover :items="items"></b-table>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: [
+				{ id: 40, tipo: "Dickerson", descripcion: "Macdonald" },
+				{ id: 21, tipo: "Larsen", descripcion: "Shaw" },
+				{ id: 89, tipo: "Geneva", descripcion: "Wilson" },
+				{ id: 38, tipo: "Jami", descripcion: "Carney" },
+			],
+		};
+	},
+};
+</script>
+
+<style>
+</style>
